964b1ba4517409e815d27f4aa18a2ed9bb28ce0c
MatinHasanpour <101702136+MatinHasanpour@users.noreply.github.com>
2024-09-05 19:31:57 +0330

    save2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import ProductsPage from "./pages/ProductsPage";
+import DetailsPage from "./pages/DetailsPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -8,8 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/products" replace />} />
         <Route path="/products" element={<ProductsPage />} />
-        <Route path="/products/:id" element={<ProductsPage />} />
-        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<DetailsPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/*" element={<PageNotFound />} />
       </Routes>
     </>
   );
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PageNotFound() {
+  return (
+    <div>
+      <h3> PageNotFound </h3> <h1> Error 404 </h1>{" "}
+    </div>
+  );
+}
+
+export default PageNotFound;
