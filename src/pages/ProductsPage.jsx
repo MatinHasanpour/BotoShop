51b4f3f504ccfdf66ef3efa6dff01af96e00c750
MatinHasanpour <101702136+MatinHasanpour@users.noreply.github.com>
2024-09-16 13:05:16 +0330

    save 5

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,21 +1,59 @@
+import { useEffect, useState } from "react";
+
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductContext";
+
 import style from "./productPage.module.css";
+import { FaListUl } from "react-icons/fa";
+import {
+  createQueryObject,
+  filterProducts,
+  getInitialQuery,
+  searchProducts,
+} from "../helper/helper";
+import { useSearchParams } from "react-router-dom";
+import SearchBox from "../components/SearchBox";
+import Sidbar from "../components/Sidbar";
 
 function ProductsPage() {
   const products = useProducts();
   console.log(products);
+
+  const [displayed, setDisplayed] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState({});
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    setDisplayed(products);
+
+    setQuery(getInitialQuery(searchParams));
+  }, [products]);
+
+  useEffect(() => {
+    setSearchParams(query);
+    setSearch(query.search || "");
+    let finalProducts = searchProducts(products, query.search);
+    finalProducts = filterProducts(finalProducts, query.category);
+
+    setDisplayed(finalProducts);
+  }, [query]);
+
   return (
-    <div className={style.container}>
-      <div className={style.products}>
-        {!products.length && <Loader />}
-        {products.map((p) => (
-          <Card key={p.id} data={p} />
-        ))}
+    <>
+      <SearchBox search={search} setQuery={setQuery} setSearch={setSearch} />
+      <div className={style.container}>
+        <div className={style.products}>
+          {!displayed.length && <Loader />}
+          {displayed.map((p) => (
+            <Card key={p.id} data={p} />
+          ))}
+        </div>
+        <Sidbar setQuery={setQuery} createQueryObject={createQueryObject} />
       </div>
-      <div>sidebar</div>
-    </div>
+    </>
   );
 }
 
