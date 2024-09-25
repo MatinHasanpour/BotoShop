18f984d961cda6b0ba2c3759993cb35d745ecb37
MatinHasanpour <101702136+MatinHasanpour@users.noreply.github.com>
2024-09-25 12:58:59 +0330

    save 10

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { Link, useParams } from "react-router-dom";
+
+import { useProductِDetails } from "../context/ProductContext";
+import Loader from "../components/Loader";
+
+import { SiOpenproject } from "react-icons/si";
+import { IoMdPricetag } from "react-icons/io";
+import { FaArrowLeft } from "react-icons/fa";
+
+import style from "./DetailsPage.module.css/";
 
 function DetailsPage() {
-  return <div>DetailsPage</div>;
+  const { id } = useParams();
+
+  const productDetails = useProductِDetails(+id);
+
+  if (!productDetails) return <Loader />;
+
+  return (
+    <div className={style.container}>
+      <img src={productDetails.image} alt={productDetails.title} />
+      <div className={style.information}>
+        <h3 className={style.title}>{productDetails.title}</h3>
+        <p className={style.description}>{productDetails.description}</p>
+        <p className={style.category}>
+          <SiOpenproject />
+          {productDetails.category}
+        </p>
+        <div>
+          <span className={style.price}>
+            <IoMdPricetag />
+            {productDetails.price} $
+          </span>
+          <Link to="/products">
+            <FaArrowLeft />
+            <span>Back to Shop</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default DetailsPage;
